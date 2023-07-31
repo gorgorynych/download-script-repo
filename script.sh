8d34fd8961126ef6a7946e992f36d1d49b5ebf43
gorgorynych <minegorynych@gmail.com>
2023-07-31 09:40:08 +0300

    Fix sh test condition

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 
 msg="Success"
 
-if [[ $(curl -o /dev/null -s -w "%{http_code}\n" $1) -ne 200 ]] ; then
+if [ $(curl -o /dev/null -s -w "%{http_code}\n" $1) -ne 200 ] ; then
   msg="Failure"
 fi
 
